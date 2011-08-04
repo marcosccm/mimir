af9ad626522c1120ba5856a1be6935ebd61673e8
marcosccm <marcosccm@gmail.com>
2011-08-04 00:00:25 -0300

    add remove books and stats view

--- public/js/Mimir.coffee
+++ public/js/Mimir.coffee
@@ -1,4 +1,6 @@
 window.Book = class Book extends Backbone.Model
+  clear: =>
+    @destroy()
 
 window.Books = class Books extends Backbone.Collection
   model: Book
@@ -10,6 +12,9 @@ window.BookView = class BookView extends Backbone.View
   tagName: "li"
   className: "book"
 
+  events:
+    "click button" : "removeItem"
+
   initialize: =>
     @template = _.template $("#book-template").html()
     @model.bind "change", @render
@@ -19,17 +24,36 @@ window.BookView = class BookView extends Backbone.View
     $(@el).html(rendered)
     this
 
+  removeItem: =>
+    @model.clear()
+    $(@el).remove()
+
 window.ReadingList = class ReadingList extends Backbone.View
+  tagName: "section"
+
+  events:
+    "blur #book-title" : "addBook"
+
   initialize: =>
     @template = _.template $("#reading-list-template").html()
+    @statsTemplate = _.template $("#stats").html()
     @collection.bind "reset", @render
-
+    @collection.bind "add", @render
+    @collection.bind "all", @renderStats
+   
   render: =>
-    $(@el).html(@template())
+    $(@el).html(@template({}))
     @collection.each (book) ->
       view = new BookView(model: book, collection:  @collection)
       @$('.reading-list').append(view.render().el)
     this
+
+  renderStats: =>
+    @$('#book-stats').html(@statsTemplate(total: @collection.length))
+
+  addBook: =>
+    book = new Book(title: $("#book-title").val())
+    @collection.add(book)
 
 window.BooksRouter = class BooksRouter extends Backbone.Router
   routes:
--- public/js/Mimir.js
+++ public/js/Mimir.js
@@ -1,18 +1,22 @@
 (function() {
   var Book, BookView, Books, BooksRouter, ReadingList;
-  var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }, __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
     for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
     function ctor() { this.constructor = child; }
     ctor.prototype = parent.prototype;
     child.prototype = new ctor;
     child.__super__ = parent.prototype;
     return child;
-  }, __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  };
   window.Book = Book = (function() {
     __extends(Book, Backbone.Model);
     function Book() {
+      this.clear = __bind(this.clear, this);
       Book.__super__.constructor.apply(this, arguments);
     }
+    Book.prototype.clear = function() {
+      return this.destroy();
+    };
     return Book;
   })();
   window.Books = Books = (function() {
@@ -28,12 +32,16 @@
   window.BookView = BookView = (function() {
     __extends(BookView, Backbone.View);
     function BookView() {
+      this.removeItem = __bind(this.removeItem, this);
       this.render = __bind(this.render, this);
       this.initialize = __bind(this.initialize, this);
       BookView.__super__.constructor.apply(this, arguments);
     }
     BookView.prototype.tagName = "li";
     BookView.prototype.className = "book";
+    BookView.prototype.events = {
+      "click button": "removeItem"
+    };
     BookView.prototype.initialize = function() {
       this.template = _.template($("#book-template").html());
       return this.model.bind("change", this.render);
@@ -44,21 +52,34 @@
       $(this.el).html(rendered);
       return this;
     };
+    BookView.prototype.removeItem = function() {
+      this.model.clear();
+      return $(this.el).remove();
+    };
     return BookView;
   })();
   window.ReadingList = ReadingList = (function() {
     __extends(ReadingList, Backbone.View);
     function ReadingList() {
+      this.addBook = __bind(this.addBook, this);
+      this.renderStats = __bind(this.renderStats, this);
       this.render = __bind(this.render, this);
       this.initialize = __bind(this.initialize, this);
       ReadingList.__super__.constructor.apply(this, arguments);
     }
+    ReadingList.prototype.tagName = "section";
+    ReadingList.prototype.events = {
+      "blur #book-title": "addBook"
+    };
     ReadingList.prototype.initialize = function() {
       this.template = _.template($("#reading-list-template").html());
-      return this.collection.bind("reset", this.render);
+      this.statsTemplate = _.template($("#stats").html());
+      this.collection.bind("reset", this.render);
+      this.collection.bind("add", this.render);
+      return this.collection.bind("all", this.renderStats);
     };
     ReadingList.prototype.render = function() {
-      $(this.el).html(this.template());
+      $(this.el).html(this.template({}));
       this.collection.each(function(book) {
         var view;
         view = new BookView({
@@ -68,6 +89,18 @@
         return this.$('.reading-list').append(view.render().el);
       });
       return this;
+    };
+    ReadingList.prototype.renderStats = function() {
+      return this.$('#book-stats').html(this.statsTemplate({
+        total: this.collection.length
+      }));
+    };
+    ReadingList.prototype.addBook = function() {
+      var book;
+      book = new Book({
+        title: $("#book-title").val()
+      });
+      return this.collection.add(book);
     };
     return ReadingList;
   })();
